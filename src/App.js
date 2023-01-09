d0b163494040fc997f065b820d6431285e26c29e
Adam Bossy <adambossy@gmail.com>
2023-01-09 12:45:43 -0500

    Add basic Product component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
+import { Component } from "react";
 import './App.css';
 import Product from "./components/Product"
 
-function App() {
-    return (
-        <div className="App">
-            <Product />
-        </div>
-    );
+class App extends Component {
+    state = {
+        products: [
+            {
+                id: 0,
+                name: "Avocado",
+                image: "avocado.webp",
+                count: 0
+            },
+            {
+                id: 1,
+                name: "Broccoli",
+                image: "broccoli.jpeg",
+                count: 0
+            },
+            {
+                id: 2,
+                name: "Cauliflower",
+                image: "cauliflower.jpeg",
+                count: 0
+            },
+            {
+                id: 3,
+                name: "Pasture-raised beef",
+                image: "beef.jpeg",
+                count: 0
+            },
+        ]
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <Product product={this.state.products[0]} />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,8 +3,18 @@ import React, { Component } from "react";
 class Product extends Component {
 
     render() {
+        const { product } = this.props;
         return (
             <div className="Product">
+                <h1 className="Product-name">
+                    {product.name}
+                </h1>
+                <img src={product.image} className="Product-image" alt={product.name} />
+                <div class="Product-cart-controls">
+                    <div class="Product-cart-add">+</div>
+                    <div class="Product-cart-count">{product.count}</div>
+                    <div class="Product-cart-remove">-</div>
+                </div>
             </div>
         );
     }
